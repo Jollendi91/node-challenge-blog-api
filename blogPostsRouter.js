f41537a6a50fb61d5e0cdcab5346a7f17fe56dd5
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-04-13 19:08:19 -0500

    add DELETE and PUT method for Blog Post router

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -6,6 +6,13 @@ const jsonParser = bodyParser.json();
 
 const{BlogPosts} = require('./models');
 
+
+BlogPosts.create('First Post', 'lorem ipsum quipsum', 'Me');
+
+BlogPosts.create('Second Post', 'This is the content for the second post. Isn\'t it great?', 'Casey');
+
+
+
 router.get('/', (req, res) => {
     res.json(BlogPosts.get());
 });
@@ -24,3 +31,38 @@ router.post('/', jsonParser, (req, res) => {
     const post = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
     res.status(201).json(post);
 });
+
+router.delete('/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post '${req.params.id}'`);
+    res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing '${field}' in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id) {
+        const message = (`Request path id (${req.params.id}) and request body id (${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    console.log(`Updating blog post '${req.params.id}'`);
+    const updatedPost = BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.budget,
+        publishDate: req.body.publishDate || Date.now()
+    });
+    res.status(204).end();
+})
+
+module.exports = router;
+
